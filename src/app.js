073a24ed51e6c6b41cd076afe02af89b2f74e9c5
Francis J <fajoaq@gmail.com>
2021-04-16 01:14:53 -0400

    Server and route setup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
 const express = require('express');
 
 const app = express();
+
+app.get('', (req, res) => {
+    res.send(`
+        <h1>Welcome</h1>
+    `);
+});
+
+app.get('/help', (req, res) => {
+    res.send({
+        name: 'Francis',
+        age: 34
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send(
+        '<h1>About</h1>'
+    )
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: '90 degrees',
+        location: 'New York'
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});

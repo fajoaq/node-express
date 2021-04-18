9f8ff0dd484bdf900f917ea98340559369334d8a
Francis J <fajoaq@gmail.com>
2021-04-18 04:40:44 -0400

    configured app partials, created footer header

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
-const publicDir = path.join(__dirname, '../public');
-
+const hbs = require('hbs');
 const app = express();
 
-app.set('view engine', 'hbs');
+//Define paths for Express config
+const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+
+//Setup handlerbars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDir));
 
 app.get('', (req, res) => {
@@ -25,14 +34,16 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        message: 'Get some help with the app.'
+        message: 'Get some help with the app.',
+        name: 'Francis J.'
     });
 });
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: '90 degrees',
-        location: 'New York'
+        location: 'New York',
+        name: 'Francis J.'
     });
 })
 
